0e0e666ffa41a93771099b9d4e90a768e3c2564f
WorldlineZero <adb.aidanbennett@gmail.com>
2018-12-19 10:47:33 -0500

    added display function to js file.

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -1,35 +1,44 @@
 function calcDisplay{
 //this is the display of the calculator (duh)
+
 }
 
 function decimalization {
 //. button - adds a decimal point after an input (past tense) number
+
 }
 
 function addition {
 //+ button - multiplies a number by another number
+
 }
 
 function subtract {
 //- button - multiplies a number by another number
+
 }
 
 function multiplication {
 //* button - multiplies a number by another number
+
 }
 
 function division {
 //÷ button - divides a number by another number
+
 }
 
 function summation {
 //= button - finds the sum of an equation
+
 }
 
 function negation {
 //+/= button - makes positive numbers negative and vice versa
+
 }
 
 function cancellation {
 //A/C button - clears current display and all memory
+
 }
