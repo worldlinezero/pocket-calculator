a6be4413206a046e6c7522a3a20ee25cd5964f57
WorldlineZero <adb.aidanbennett@gmail.com>
2019-01-04 10:43:58 -0500

    added functions for number keys.

--- pocket-calculator.js
+++ pocket-calculator.js
@@ -44,3 +44,43 @@ const canvas = document.getElementById("calcDisplay");
 const ctx = canvas.getContext("2d");
 ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
+
+function one {
+
+}
+
+function two {
+
+}
+
+function three {
+
+}
+
+function four {
+
+}
+
+function five {
+
+}
+
+function six {
+
+}
+
+function seven {
+
+}
+
+function eight {
+  
+}
+
+function nine {
+
+}
+
+function zero {
+
+}
